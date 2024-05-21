d11120cbfa7a43dc6a3ec6ee06cd89e6f1e8815c
Max Lennon D Limpag <lennon@limpag.com>
2024-05-21 14:43:58 +0800

    Add initial main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,6 @@
-"#include <iostream>\n\nint main() {\n    std::cout << 'Hello, World!' << std::endl;\n    return 0;\n}" 
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
